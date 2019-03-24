ced03834b8a43923b228a15875e9141bf0a71934
Joshua 'Squid' Kauer <joshuakauer@gmail.com>
2019-03-24 15:14:30 -0600

    Adding tiny_obj_loader and an obj model with texture

--- Engine/RenderEngine.cpp
+++ Engine/RenderEngine.cpp
@@ -69,7 +69,6 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT
 	}
 }
 
-
 std::vector<Vertex> vertices =
 {
 	{ { -0.5f, -0.5f, 0.0f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 0.0f } },
