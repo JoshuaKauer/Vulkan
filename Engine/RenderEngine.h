164d34be1e9751378873adf8cb85202dbcfd1b97
Joshua 'Squid' Kauer <joshuakauer@gmail.com>
2019-03-30 21:01:16 -0600

    Removing commented code that was moved to Texture class

--- Engine/RenderEngine.h
+++ Engine/RenderEngine.h
@@ -63,13 +63,6 @@ private:
 	VkDescriptorPool descriptorPool;
 	std::vector<VkDescriptorSet> descriptorSets;
 
-	/*uint32_t mipLevels;
-	VkImage textureImage;
-	VkDeviceMemory textureImageMemory;
-
-	VkImageView textureImageView;
-	VkSampler textureSampler;*/
-
 	Texture texture;
 
 	VkImage depthImage;
@@ -114,15 +107,6 @@ private:
 	VkFormat FindDepthFormat();
 	VkFormat FindSupportedFormat(const std::vector<VkFormat>& candidates, VkImageTiling tiling, VkFormatFeatureFlags features);
 	bool HasStencilComponent(VkFormat format);
-
-	/*void CreateTextureImage();
-	void CreateTextureImageView();
-	VkImageView CreateImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, uint32_t mipLevels);
-	void CreateImage(uint32_t width, uint32_t height, uint32_t mipLevels, VkSampleCountFlagBits numSamples, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
-	void TransitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels);
-	void CopyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height);
-	void CreateTextureSampler();
-	void GenerateMipmaps(VkImage image, VkFormat imageFormat, int32_t texWidth, int32_t texHeight, uint32_t mipLevels);*/
 
 	VkSampleCountFlagBits GetMaxUsableSampleCount();
 
